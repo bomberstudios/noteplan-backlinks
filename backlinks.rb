3c7c6f53a981b31db21b20649e4309b95e911aae
Ale M <bomberstudios@gmail.com>
2020-05-12 16:17:14 +0200

    Backlinks are working

--- backlinks.rb
+++ backlinks.rb
@@ -1,6 +1,7 @@
 # Backlink Builder
 
-PATH_TO_NOTEPLAN = "/Users/ale/Library/Mobile Documents/iCloud~co~noteplan~NotePlan/Documents"
+PATH_TO_NOTEPLAN = "."
+# PATH_TO_NOTEPLAN = "/Users/ale/Library/Mobile Documents/iCloud~co~noteplan~NotePlan/Documents"
 PATH_TO_NOTES = "#{PATH_TO_NOTEPLAN}/Notes"
 PATH_TO_CALENDAR = "#{PATH_TO_NOTEPLAN}/Calendar"
 
@@ -17,34 +18,86 @@ def links_from_text text
   regex.match(text)
 end
 
-def title_from_note_file file
-  filename = File.basename(file)
-  # Is the title the first line, or the filename?
-  # Turns out it's the first line, minus
+def file_contents file
+  File.read(file)
 end
 
-all_note_files.each do |file|
-  filename = File.basename(file)
-  note_title = File.basename(filename)
+def title_from_note_file file
+  first_line = lines_from_file(file).first
+  title = first_line.gsub("# ","").strip
+  return title
+  # Is the title the first line, or the filename?
+  # Turns out it's the first line, minus the header cruft
+end
 
-  all_content = File.read(file)
-
-  if links_from_text all_content
-    puts "----"
-    puts "# Links in page #{note_title}"
-    puts
-    # puts "Checking #{File.basename(file)} for links"
-    lines_from_file(file).each do |line|
-      links = links_from_text(line)
-      # TODO: remove "- " and other crap from the beginning of the line,
-      # *if* we want to use the line for anything (maybe we don't)
-      # line = line.gsub("- ","")
-      if links
-        link = links[1]
-        link_title = links[2]
-        puts "- #{link}"
-      end
-    end
-    puts "----"
+def has_links file
+  if links_from_text(File.read(file))
+    return true
+  else
+    return false
   end
 end
+
+def links_from_file file
+  file_links = []
+  lines_from_file(file).each do |line|
+    links = links_from_text(line)
+    if links
+      file_links.push(links)
+    end
+  end
+  return file_links
+end
+
+def update_backlinks_block(file, links)
+  backlink_block_regex = /\n\n♻︎ Backlinks\n(.+\n)+♻︎ Backlinks/
+
+  backlink_block = "\n\n♻︎ Backlinks\n"
+  links.sort.each do |link|
+    backlink_block << "- #{link}\n"
+  end
+  backlink_block << "♻︎ Backlinks\n"
+
+  contents = file_contents(file)
+
+  if contents.match(backlink_block_regex)
+    puts "File already has a backlinks block, updating"
+    contents.gsub(backlink_block_regex, backlink_block)
+  else
+    puts "File has no backlinks block, creating"
+    contents << backlink_block
+  end
+
+  File.open(file, 'w') do |f|
+    f << contents
+  end
+
+end
+
+link_database = Hash.new()
+
+all_note_files.each do |file|
+  # 1. Build a database of all links in all pages
+  note_title = title_from_note_file(file)
+  if has_links(file)
+    links = links_from_file(file)
+    links.each do |link|
+      link_full = link[1]
+      link_title = link[2]
+      if !link_database[note_title]
+        link_database[note_title] = []
+      end
+      link_database[note_title].push(link_full)
+    end
+  end
+  # 2. For each page, check the database for links that point at that page
+  links_to_page = link_database[note_title]
+  if links_to_page
+    puts note_title
+    # 3. Bake backlinks in note file, replacing existing Backlinks (otherwise you end up with repeated backlinks)
+    update_backlinks_block(file, links_to_page)
+  end
+end
+
+# puts link_database
+
