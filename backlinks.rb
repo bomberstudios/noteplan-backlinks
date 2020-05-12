2bafb9a4e9d97cc4415045a6a82af913ddbcffd5
Ale M <bomberstudios@gmail.com>
2020-05-12 16:53:14 +0200

    Small refactor

--- backlinks.rb
+++ backlinks.rb
@@ -6,6 +6,10 @@ PATH_TO_NOTEPLAN = "."
 PATH_TO_NOTES = "#{PATH_TO_NOTEPLAN}/Notes"
 PATH_TO_CALENDAR = "#{PATH_TO_NOTEPLAN}/Calendar"
 
+REGEX_LINK = /(\[\[([^\]]+)\]\])/
+# REGEX_LINK = Regexp.new(/(\[\[([^\]]+)\]\])/, Regexp::MULTILINE)
+REGEX_BACKLINKS = /\n\n#{BACKLINKS_MARKER}\n(.+\n)+#{BACKLINKS_MARKER}/
+
 def all_note_files
   Dir.glob("#{PATH_TO_NOTES}/**.{txt,md}").sort
 end
@@ -15,8 +19,7 @@ def lines_from_file file
 end
 
 def links_from_text text
-  regex = /(\[\[([^\]]+)\]\])/
-  regex.match(text)
+  REGEX_LINK.match(text)
 end
 
 def file_contents file
@@ -27,32 +30,29 @@ def title_from_note_file file
   first_line = lines_from_file(file).first
   title = first_line.gsub("# ","").strip
   return title
-  # Is the title the first line, or the filename?
-  # Turns out it's the first line, minus the header cruft
 end
 
 def has_links file
-  if links_from_text(File.read(file))
-    return true
-  else
-    return false
-  end
+  REGEX_LINK.match?(File.read(file))
 end
 
 def links_from_file file
+  contents = file_contents(file)
+  # obviously, we need to ignore the Backlinks block!
+  contents.gsub!(REGEX_BACKLINKS,"")
+
   file_links = []
-  lines_from_file(file).each do |line|
-    links = links_from_text(line)
+  contents.split("\n").each do |line|
+    links = REGEX_LINK.match(line)
     if links
-      file_links.push(links)
+      file_links.push(links[1])
     end
   end
   return file_links
+
 end
 
 def update_backlinks_block(file, links)
-  backlink_block_regex = /\n\n#{BACKLINKS_MARKER}\n(.+\n)+#{BACKLINKS_MARKER}/
-
   backlink_block = "\n\n#{BACKLINKS_MARKER}\n"
   links.sort.each do |link|
     backlink_block << "- #{link}\n"
@@ -61,9 +61,9 @@ def update_backlinks_block(file, links)
 
   contents = file_contents(file)
 
-  if contents.match(backlink_block_regex)
+  if contents.match(REGEX_BACKLINKS)
     puts "File already has a backlinks block, updating"
-    contents.gsub(backlink_block_regex, backlink_block)
+    contents.gsub(REGEX_BACKLINKS, backlink_block)
   else
     puts "File has no backlinks block, creating"
     contents << backlink_block
@@ -80,21 +80,21 @@ link_database = Hash.new()
 all_note_files.each do |file|
   # 1. Build a database of all links in all pages
   note_title = title_from_note_file(file)
+
   if has_links(file)
+    puts "Finding links in #{note_title}"
     links = links_from_file(file)
     links.each do |link|
-      link_full = link[1]
-      link_title = link[2]
       if !link_database[note_title]
         link_database[note_title] = []
       end
-      link_database[note_title].push(link_full)
+      link_database[note_title].push(link)
     end
   end
   # 2. For each page, check the database for links that point at that page
   links_to_page = link_database[note_title]
   if links_to_page
-    puts note_title
+    # puts note_title
     # 3. Bake backlinks in note file, replacing existing Backlinks (otherwise you end up with repeated backlinks)
     update_backlinks_block(file, links_to_page)
   end
